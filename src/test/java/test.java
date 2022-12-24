b90195acb6d6ca356a86cbd5aae7110a79376986
hakankob0@gmail.com <sezer.0734@hotmail.com>
2022-12-24 10:05:56 +0300

    sezer

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,17 +2,7 @@ import org.testng.annotations.Test;
 
 public class test {
     /*
-    test
-    sdd
-    ewsdxc
-    sds
-    s
-    GF
-    <zxxx
-    ad
-    gff
-    sdsx
-    ds
+    Team16
 
      */
 
